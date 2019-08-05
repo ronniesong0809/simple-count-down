89e9ae2f88e483674b97b8d5791935620f78a810
ronniesong0809 <ronniesong0809@gmail.com>
2019-08-05 02:41:25 -0700

    doc: update README.md, upload License and looping the count-down

--- count_down.js
+++ count_down.js
@@ -2,16 +2,17 @@ function animation(n) {
   $('#countdown').fadeIn('fast', function () {
     if ($(this).html() == "" || $(this).html() == 3) {
       $(this).html(n);
-      
     }
-  
+
     $('#countdown').delay(600).hide('puff', 400, function () {
       if (n == 0) {
         blinking();
+        n = 3;
+        $('#countdown').show();
+      }
 
-      } else {
+      else {
         n--;
-
       }
 
       $(this).html(n);
@@ -22,14 +23,13 @@ function animation(n) {
 
 function blinking() {
   $("body").css("background-color", 'grey');
-  
   setTimeout(function () {
     $("body").css("background-color", 'white');
-
   }, 300);
+
+  $('#countdown').hide();
 }
 
 $(function () {
   animation(3);
-
 });
